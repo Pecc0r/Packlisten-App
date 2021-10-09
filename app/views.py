cb90fb9407bb950256a9858b8f1720754215ebfb
Pecc0r <rebecca.husemann@gmail.com>
2021-10-09 13:10:41 +0200

    API endpoint dummy added

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,flash, redirect, request, jsonify
+from flask import render_template,flash, redirect, request, jsonify, Response
 from app import app
 import json
 import os
@@ -8,3 +8,12 @@ def index():
     return render_template("index.html",
                            title='VSUP - Meteogram',
                            )
+
+@app.route('/api/packliste', methods=("POST",))
+def packliste():
+    payload=request.get_json(force = True)
+    print(payload)
+    return Response(
+      json.dumps({}),
+      mimetype = "application/json"
+    )
