c4f8cc94b246492a7ca90121daf85ce96a113bab
Pecc0r <rebecca.husemann@gmail.com>
2021-10-10 23:16:25 +0200

    added slider function

--- app/views.py
+++ app/views.py
@@ -15,6 +15,7 @@ def index():
 def packliste():
     payload=request.get_json(force = True)
     print(payload)
+    payload["slider"] = int(payload["slider"])
     packliste = {
       "Infrastruktur":[],
       "Kleidung":[],
@@ -24,7 +25,7 @@ def packliste():
     if "ultralight" in payload["checkboxes"]:
         if "fahrrad" in payload["checkboxes"] and "wandern" in payload["checkboxes"]:
            packliste["Infrastruktur"] += ["UL-Zelt", "Fahrrad", "Fahrradhelm", "Wanderstöcke", "Wanderrucksack", "Schlafsack", "Luftmatratze", "Heringe", "Kissen", "Iso-Sitzmatte", "Stirnlampe", "Powerbank", "Stempelhefte"]
-           packliste["Kleidung"] += ["Buff", "Sporthose", "Wandersocken", "Sportkleid", "Merinopulli", "BH", "Unterhosen", "Wanderschuhe", "Jacke/Cape", "Sonnenbrille"]
+           packliste["Kleidung"] += ["Buff", "Sporthose", "Wandersocken", "Sportkleid", "Merinopulli", "BH", "3 Unterhosen", "Wanderschuhe", "Jacke/Cape", "Sonnenbrille"]
            packliste["Kochen"] += ["UL-Kocher", "X-Bowl", "Titanlöffel", "Hauptmahlzeiten", "Wasserflaschen", "Trailmix"]
            packliste["Hygiene"] += ["Pflaster", "Sonnencreme", "Bürste", "Deo", "Zahnbürste", "UL-Handtuch", "Schminke"]
         elif "fahrrad" in payload["checkboxes"]:
@@ -36,22 +37,35 @@ def packliste():
            packliste["Infrastruktur"] += ["UL-Zelt", "Wanderstöcke", "Wanderrucksack", "Schlafsack", "Isomatte", "Heringe", "Kissen", "Iso-Sitzmatte"]
            packliste["Kleidung"] += ["Buff", "Sporthose", "Wandersocken", "Sportkleid", "Merinopulli", "BH", "Unterhosen"]
            packliste["Kochen"] += ["UL-Kocher", "X-Bowl", "Titanlöffel", "Hauptmahlzeiten", "Wasserflaschen", "Trailmix"]
-           packliste["Hygiene"] += ["Hörgeräte", "Bürste", "Deo", "Zahnbürste", "UL-Handtuch", "Schminke"]
+           packliste["Hygiene"] += ["Hörgeräte", "Bürste", "Deo", "Zahnbürste", "Handtuch", "Schminke"]
     else:
         if "wandern" in payload["checkboxes"]:
             packliste["Infrastruktur"] += ["Wanderstöcke", "Wanderrucksack", "Stempelhefte"]
         if "fahrrad" in payload["checkboxes"]:
             packliste["Infrastruktur"] += ["Fahrrad", "Fahrradhelm"]
+        if "auto" in payload["checkboxes"]:
+             packliste["Infrastruktur"] += ["Schlafsack", "Isomatte", "Kissen", "Stuhl"]
+             packliste["Kochen"] += ["Gaskocher", "Infrastrukturbox", "Hauptmahlzeiten", "Wasserflaschen"]
+             packliste["Hygiene"] += ["Hörgeräte", "Bürste", "Deo", "Zahnbürste", "UL-Handtuch", "Schminke"]
+             if not "zelten" in payload["checkboxes"]:
+                 payload["checkboxes"].append("zelten")
+        if "zelten" in payload["checkboxes"]:
+            packliste["Infrastruktur"] += ["Zelt", "Heringe"]
         if "camper" in payload["checkboxes"]:
             packliste["Infrastruktur"] += ["Palettenkissen", "Heckzelt", "Kissen", "Decke", "Stühle"]
-            packliste["Kleidung"] += ["Buff", "Schlafsachen", "Socken", "BH", "Unterhosen"]
+            packliste["Kleidung"] += ["Buff", "Schlafsachen", "Socken", "BH"]
             packliste["Kochen"] += ["Gaskocher", "Gas", "Titanlöffel", "Hauptmahlzeiten", "Wasserflaschen", "Trailmix"]
             if "fahrrad" in payload["checkboxes"]:
-               packliste["Infrastruktur"] += ["Fahrradträger"]
+                packliste["Infrastruktur"] += ["Fahrradträger"]
         if "glamping" in payload["checkboxes"]:
             if "camper" in payload["checkboxes"]:
-               packliste["Infrastruktur"] += ["Teppich", "Heizung"]
-               packliste["Kleidung"] += ["Kleider",]
+                packliste["Infrastruktur"] += ["Teppich", "Heizung"]
+                packliste["Kleidung"] += ["Kleider",]
+        if payload["slider"] > 1:
+            if "glamping" in payload["checkboxes"]:
+                packliste["Kleidung"].append(f"{payload['slider'] * 2} Unterhosen")
+            else:
+                packliste["Kleidung"].append(f"{payload['slider'] + 2} Unterhosen")
     return Response(
       json.dumps(packliste),
       mimetype = "application/json"
